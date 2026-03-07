aa48739f7afaf6195105a224600cba7075191174
Darshit Mistry <darshit.code@gmail.com>
2026-03-07 11:08:50 +0530

    FEAT : data loader initialize

---
+++ data/data_loader.py
@@ -0,0 +1,20 @@
+import pandas as pd 
+import yaml
+
+def load_config(path="config/config.yaml"):
+    with open(path , "r") as file:
+        config = yaml.safe_load(file)
+    return config
+
+
+def load_dataset():
+    config = load_config()
+    data_path = config["data"]["raw_data_path"]
+    df = pd.read_csv(data_path)
+    return df
+
+if __name__ == "__main__" :
+    df = load_dataset()
+    print("DataSet Loaded successfully")
+    print(df.head())
+    print("Shape:" , df.shape)
